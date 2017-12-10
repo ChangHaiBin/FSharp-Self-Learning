efc8516ab36584e989330fea4f24a86a790de06f
HaiBin <haibinmath@gmail.com>
2017-12-10 16:41:57 +0800

    Basic code.

---
+++ Chapter7/Chapter7/DataTypes.fs
@@ -0,0 +1,30 @@
+﻿module DataTypes
+
+open System.Drawing
+
+type Rect =
+    {
+        Left    : float32
+        Top     : float32
+        Width   : float32
+        Height  : float32
+    }
+type TextContent =
+    { Text : string
+      Font : Font}
+type ScreenElement =
+    | TextElement of TextContent * Rect
+    | ImageElement of string * Rect     // image filename
+
+
+module Rect =
+    let deflate (original,wspace,hspace) =
+        {
+            Left = original.Left + wspace
+            Top = original.Top + hspace
+            Width = original.Width - (2.0f * wspace)
+            Height = original.Height - (2.0f * hspace)
+        }
+    
+    let toRectangleF (original) =
+        RectangleF(original.Left,original.Top,original.Width,original.Height)
---
+++ Chapter7/Chapter7/TextbookCode.fs
@@ -0,0 +1,31 @@
+﻿module TextbookCode
+
+open System.Drawing
+open DataTypes
+
+let rc = {
+    Left = 10.0f;
+    Top = 10.0f;
+    Width = 100.0f;
+    Height = 200.0f;
+    }
+    
+let rc2 = {rc with Left = rc.Left + 100.0f}
+
+let it1 = {Left = 0.0f; Top = 0.0f; Width = 100.0f; Height = 100.0f}
+
+let it2 = Rect.deflate(it1,20.0f,10.0f)
+
+//////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
